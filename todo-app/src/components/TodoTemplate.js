df55f26c6d87fa2ffdf138b46c12b59536ee21e3
JasonKK <hyeon7427@gmail.com>
2021-08-01 18:57:42 +0900

    Changed

--- todo-app/src/components/TodoTemplate.js
+++ todo-app/src/components/TodoTemplate.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './TodoTemplate.scss';
 
-const TdoTemplate = ({ children }) => {
+const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
       <div className="app-title">일정관리</div>
