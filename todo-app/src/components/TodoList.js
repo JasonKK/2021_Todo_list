127bbda4d24ac156436c0c276f276f9a042470b1
JasonKK <hyeon7427@gmail.com>
2021-08-01 21:21:22 +0900

    using map, render

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -2,13 +2,25 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = () => {
+// const TodoList = () => {
+//   return (
+//     <div className="TodoList">
+//       <TodoListItem />
+//       <TodoListItem />
+//       <TodoListItem />
+//       <TodoListItem />
+//     </div>
+//   );
+// };
+
+// export default TodoList;
+
+const TodoList = ({ Todos }) => {
   return (
     <div className="TodoList">
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
+      {Todos.map((todo) => (
+        <TodoListItem todo={todo} key={todo.id} />
+      ))}
     </div>
   );
 };
